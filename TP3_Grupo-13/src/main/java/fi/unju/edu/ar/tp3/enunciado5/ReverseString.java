688a9497ec127eb6343d7065db2c410aad41cf7c
PC <PC@Gustavo>
2022-04-24 20:48:05 -0300

    Actualizacion Enunciado 5.

--- TP3_Grupo-13/src/main/java/fi/unju/edu/ar/tp3/enunciado5/ReverseString.java
+++ TP3_Grupo-13/src/main/java/fi/unju/edu/ar/tp3/enunciado5/ReverseString.java
@@ -8,9 +8,9 @@ import org.apache.logging.log4j.util.Chars;
 public class ReverseString {
 
 	public static void main(String[] args) {
+	   	 
+		char [] caracteres = new char [999];
 		
-		ArrayList<Chars> caracteres = new ArrayList <Chars>();
-   	 
 	    Scanner entrada = new Scanner(System.in);
 	    
 	    System.out.print("Ingrese cadena a invertir:");
@@ -18,13 +18,19 @@ public class ReverseString {
 	    String cadena = entrada.nextLine();
 	    
 	    int L = cadena.length();
-		
+	    
+	    for(int i = 0; i < L; i++) {
+	    	
+	    	caracteres[i]=cadena.charAt(i);
+	    	
+	    }
+	    
 	    System.out.print("\nCadena Invertida : ");
 	    
 	    for(int i = L-1; i >= 0; i--) {
-	    	
-	    	System.out.print(cadena.charAt(i));
-	    	
+    	
+    	System.out.print(caracteres[i]);
+   	    
 	    }
 	    
 	}
